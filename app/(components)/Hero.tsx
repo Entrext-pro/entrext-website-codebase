ee25fab215c5d647d289eaa684e06d70367da7ce
Aryan Saxena <aryansaxena@gmail.com>
2025-05-20 19:09:36 +0530

    replace animation with picture

--- app/(components)/Hero.tsx
+++ app/(components)/Hero.tsx
@@ -1,52 +1,26 @@
 "use client";
 
-import { motion } from "framer-motion";
-import { MagicCard } from "@/components/magicui/magic-card";
-import {AnimatedBeamMultipleOutputDemo} from "@/app/(components)/Animation"
+import { motion, useScroll, useTransform } from "framer-motion";
+import Notebook from "@/public/notebook.jpg"
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 const line1 = "ENTERING";
 const line2 = "THE NEXT ERA";
 
-const sentences = [
-    "We grow leaders—not hire them.",
-    "We want 100 people to love what we build.",
-    "We learn fast by doing, failing, and adapting.",
-    "We're just one call away—not hidden behind systems.",
-    "Partnership with non-tech founders.",
-    "Got an idea but no tech partner? Pitch us.",
-    "Partner with us if you're a creator with product ideas.",
-    "We love content creators who resonate with our vision.",
-    "We're looking for leaders in sales, tech, or branding.",
-    "Let’s build together as co-founders—not just employees.",
-];
-const positions = [
-    ["20%", "20%"],
-    ["80%", "25%"],
-    ["20%", "60%"],
-    ["75%", "70%"],
-    ["40%", "10%"],
-    ["60%", "80%"],
-    ["5%", "50%"],
-    ["80%", "50%"],
-    ["30%", "85%"],
-    ["60%", "25%"],
-];
-
-const floatVariants = {
-    animate: (i: number) => ({
-        y: [0, -10, 0, 10, 0],
-        x: [0, i % 2 === 0 ? 10 : -10, 0, i % 2 === 0 ? -10 : 10, 0],
-        transition: {
-            repeat: Infinity,
-            duration: 8,
-            ease: "easeInOut",
-            delay: i * 0.5,
-        },
-    }),
-};
-
-
 export default function Hero() {
+    const ref = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start end", "end start"],
+    });
+    const isMobile = useIsMobile();
+    const height = useTransform(
+        scrollYProgress,
+        [0, 1],
+        isMobile ? ["300px", "100px"] : ["900px", "400px"]
+    );
+
     return (
         <div>
             <div className="mt-[90px] md:h-[500px] h-[120px] overflow-hidden">
@@ -107,51 +81,34 @@ export default function Hero() {
                 </div>
             </div>
 
-            <MagicCard className="md:block hidden border border-dashed rounded-2xl ">
-                <div className=" border border-dashed m-3 rounded-md">
-                    <div className="relative w-full h-[600px] flex items-center justify-center bg-white overflow-hidden">
-                        <motion.div
-                            className="absolute z-10 text-black font-extrabold text-4xl px-6 py-3 rounded-full bg-gray-100 animate-border-green border-2 border-transparent "
-                            initial={{ scale: 0 }}
-                            animate={{ scale: 1 }}
-                            transition={{ type: "spring", stiffness: 100, damping: 10 }}
-                        >
-                            Entrext
-                        </motion.div>
-                        {sentences.map((text, i) => (
-                            <motion.div
-                                key={i}
-                                className="absolute w-[180px] text-center px-3 py-2 rounded-lg bg-slate-100 text-black text-xs md:text-sm font-medium animate-border-black border-2 border-transparent cursor-pointer"
-                                style={{
-                                    top: positions[i][1],
-                                    left: positions[i][0],
-                                }}
-                                custom={i}
-                                variants={floatVariants}
-                                animate="animate"
-                                whileHover={{
-                                    width: 240,
-                                    height: 90,
-                                    fontSize: "18px",
-                                    transition: {
-                                        duration: 0.3,
-                                        ease: "easeInOut",
-                                    },
-                                }}
-                            >
-                                {text}
-                            </motion.div>
-
-                        ))}
-                    </div>
-                </div>
-            </MagicCard>
-
-            <div className="block md:hidden">
-                    <AnimatedBeamMultipleOutputDemo/>
+            <div className="block border border-dashed ">
+                <motion.div
+                    ref={ref}
+                    className="block border border-dashedoverflow-hidden"
+                    style={{ height }}
+                >
+                    <Image
+                        className="w-full h-full object-cover"
+                        alt="Notebook"
+                        src={Notebook}
+                    />
+                </motion.div>
             </div>
         </div>
     );
+}
+
+function useIsMobile() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkScreen = () => setIsMobile(window.innerWidth < 768);
+        checkScreen();
+        window.addEventListener("resize", checkScreen);
+        return () => window.removeEventListener("resize", checkScreen);
+    }, []);
+
+    return isMobile;
 }
 
 function getInitialDirection(char: string) {
